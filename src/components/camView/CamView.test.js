becb1bfb27d00e631eb625a094b1561c44428607
akmoore85 <akmoore85@gmail.com>
2017-08-15 21:37:52 -0400

    from alex laptop

--- src/components/camView/CamView.test.js
+++ src/components/camView/CamView.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import CamView from './Camview'
+import CamView from './CamView'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
